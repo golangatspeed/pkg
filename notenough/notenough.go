8620ec3cc9a953cbc091820a45f63c3fc9b31704
Ollie Phillips <ollie@interject.io>
2020-05-29 12:22:05 +0100

    force version update?

--- notenough/notenough.go
+++ notenough/notenough.go
@@ -9,6 +9,7 @@ func(a *MyApp) setName(name string){
 }
 
 func (a *MyApp) SimpleMethod() string {
+	// comment here
 	a.setName("Golang At Speed")
 	return a.name + " the API of notenough only comprises a really simple exported method"
 }
